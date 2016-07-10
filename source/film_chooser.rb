0317bb247e87dd943619c1a314b85ed5a1d827af
caltonhill <macdonald.james@gmail.com>
2016-07-10 20:49:00 +0100

    Improved to use .xls file with Spreadsheet gem.

--- source/film_chooser.rb
+++ source/film_chooser.rb
@@ -1,24 +1,31 @@
-# need to make work with xlsx file
-require 'csv'
-
-current_film_list = "C:\\Users\\James\\Google Drive\\films.csv"
+require 'spreadsheet'
 
 puts "----------------------------"
 puts "Hooray! The weekend is here."
 puts "A chance to watch a film."
 puts "----------------------------"
 
-# get each line/film from the file and put it into an array
+# Use Spreadsheet to load file
+current_film_list = Spreadsheet.open (open('C:\Users\James\Google Drive\films.xls'))
+
+#Target first column of first worksheet.
+
+sheet1 = current_film_list.worksheet 0
+column = sheet1.column(0)
+
+#Iterate over cells in column. If cell is populated add contents to array
 films_array = []
-CSV.foreach(current_film_list) do |row1|
-	films_array << row1
+
+column.each do |row|
+  unless row.nil?
+  films_array << row
+    end
 end
 
 # display the number of films (array size)
 puts
-puts "The number of films waiting to be watched is #{films_array.size}."
+puts "The number of films waiting to be watched is #{films_array.size}"
 puts
-
 
 # Here could ask user if they want to see a list of all films (y/n)
 puts "Do you want to see a list of all the films? (y/n)"
